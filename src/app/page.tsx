46e9ea57b45a83e91dfdaeae4893165633d7148c
Bablu Verma AmbrHomes <babluambrhomes@gmail.com>
2026-09-23 17:05:10 +0530

    remove test case in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Hello from "@/components/hello";
+
 
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-white text-zinc-900">
-      <Hello name='pgl' />
+      hello
     </div>
   );
 }
