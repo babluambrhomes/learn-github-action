7e3edaadb031e206419683df083bb0482607f4b6
Bablu Verma AmbrHomes <babluambrhomes@gmail.com>
2026-09-24 10:55:19 +0530

    learn .env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white text-zinc-900">
+    <div className="min-h-screen text-5xl bg-white text-zinc-900">
       hello
     </div>
   );
